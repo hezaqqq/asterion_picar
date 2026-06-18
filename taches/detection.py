b3553a423ede8ee3816e57ebae737f50bb65a83e
Félix Lang <felixlang1004@gmail.com>
2026-06-18 15:41:05 +0200

    add around

--- taches/detection.py
+++ taches/detection.py
@@ -1,7 +1,9 @@
 import robot_controller as robot_controller
 import ultrasonic_sensor as ultrasonic_sensor
 import servo_controller as servo
+
 import time
+import threading
 
 ANGLE_CENTER_ROUE    = 100
 ANGLE_MIN_ROUE       = 60
@@ -11,27 +13,24 @@ ANGLE_CENTER_TETE_GD = 108
 ANGLE_MIN_TETE_GD    = 48
 ANGLE_MAX_TETE_GD    = 168
 
-OBSTACLE_DIST_CM     = 30    # distance seuil pour évitement
-STEER_DELTA          = 20    # degrés de braquage roues
-STEER_HOLD_TICKS     = 10    # combien de ticks on maintient le braquage (~0.5s)
-
 if __name__ == "__main__":
     robot = None
     try:
         sensor = ultrasonic_sensor.UltrasonicSensor()
-        robot  = robot_controller.RobotController(sensor=sensor)
+        robot = robot_controller.RobotController(sensor=sensor)
         servos = servo.ServoController()
 
         servos.set_angle(1, ANGLE_CENTER_TETE_GD)
         servos.set_angle(2, 75)
         servos.set_angle(0, ANGLE_CENTER_ROUE)
 
-        gauche        = True
+        gauche       = True
         angle_tete_gd = ANGLE_CENTER_TETE_GD
-        steer_angle   = ANGLE_CENTER_ROUE
-        steer_hold    = 0 
+        around = False
 
         while True:
+            # Balayage tête
+            
             if gauche:
                 angle_tete_gd += 1
                 if angle_tete_gd >= ANGLE_MAX_TETE_GD:
@@ -40,25 +39,25 @@ if __name__ == "__main__":
                 angle_tete_gd -= 1
                 if angle_tete_gd <= ANGLE_MIN_TETE_GD:
                     gauche = True
+            
+            if sensor.get_distance_mm() < 150 and around == False:
+                angle_before_around = angle_tete_gd
+                if 48 <= angle_tete_gd <=108:
+                    servos.set_angle(0, ANGLE_CENTER_ROUE-20)
+                    time.sleep(3)
+                    servos.set_angle(0, ANGLE_CENTER_ROUE+20)
+                    time.sleep(3)
+                    servos.set_angle(0, ANGLE_CENTER_ROUE) 
+                
+                else:
+                    servos.set_angle(0, ANGLE_CENTER_ROUE+20)
+                    time.sleep(3)
+                    servos.set_angle(0, ANGLE_CENTER_ROUE-20)
+                    time.sleep(3)
+                    servos.set_angle(0, ANGLE_CENTER_ROUE) 
+                    
+
             servos.set_angle(1, angle_tete_gd)
-
-            dist = sensor.get_distance_mm()
-
-            if dist is not None and dist < OBSTACLE_DIST_CM:
-                # Obstacle donc on va vers le côté opposé à la tête
-                if angle_tete_gd < ANGLE_CENTER_TETE_GD:
-                    steer_angle = min(ANGLE_MAX_ROUE, ANGLE_CENTER_ROUE + STEER_DELTA)
-                else:
-                    steer_angle = max(ANGLE_MIN_ROUE, ANGLE_CENTER_ROUE - STEER_DELTA)
-                steer_hold = STEER_HOLD_TICKS
-            else:
-                # Pas d'obstacle
-                if steer_hold > 0:
-                    steer_hold -= 1
-                else:
-                    steer_angle = ANGLE_CENTER_ROUE
-
-            servos.set_angle(0, steer_angle)
             time.sleep(0.05)
 
     except KeyboardInterrupt:
@@ -66,4 +65,3 @@ if __name__ == "__main__":
             robot.stop()
             robot.hazard_off()
         servos.set_angle(1, ANGLE_CENTER_TETE_GD)
-        servos.set_angle(0, ANGLE_CENTER_ROUE)
