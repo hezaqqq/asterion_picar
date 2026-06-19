763cd0eab256fdb7a54820462583153bfb5be290
Félix Lang <felixlang1004@gmail.com>
2026-06-19 09:39:03 +0200

    chnage speed

--- taches/stay_zone.py
+++ taches/stay_zone.py
@@ -14,8 +14,8 @@ class StayInZone:
     ANGLE_MIN_TETE_GD    = 18
     ANGLE_MAX_TETE_GD    = 198
 
-    SPEED_STRAIGHT = 0.25
-    SPEED_CURVE    = 0.20
+    SPEED_STRAIGHT = 0.1
+    SPEED_CURVE    = 0.10
 
     REVERSE_TIME   = 2
     RAMP_TIME      = 0.2
