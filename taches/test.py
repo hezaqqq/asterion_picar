b90e6805a12a4019dce731bcc2daeb81a3ca5970
Félix Lang <felixlang1004@gmail.com>
2026-06-22 15:14:38 +0200

    change test angles

--- taches/test.py
+++ taches/test.py
@@ -7,7 +7,7 @@ time.sleep(1)
 print("Demandé: 90")
 input("Vérifie visuellement la position, puis Entrée...")
 
-s.set_angle(1, 90+72)
+s.set_angle(1, 18)
 time.sleep(1)
 print("Demandé: 54")
 input("Vérifie visuellement, puis Entrée...")
