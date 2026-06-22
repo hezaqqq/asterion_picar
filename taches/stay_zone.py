1134e7669e4bce3152a8a1d168728dd2e16d807b
Félix Lang <felixlang1004@gmail.com>
2026-06-22 15:44:38 +0200

    change distance detection

--- taches/stay_zone.py
+++ taches/stay_zone.py
@@ -20,7 +20,7 @@ class StayInZone:
     REVERSE_TIME   = 2.25
     RAMP_TIME      = 0.2
 
-    OBSTACLE_DIST_MM = 250
+    OBSTACLE_DIST_MM = 200
 
     def __init__(self):
         self.sensor    = ultrasonic_module.UltrasonicSensor()
