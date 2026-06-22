0af26dc343895cd51d3cff011affbb633310b4d5
Félix Lang <felixlang1004@gmail.com>
2026-06-22 15:14:04 +0200

    change safe angles

--- taches/servo_controller.py
+++ taches/servo_controller.py
@@ -8,7 +8,7 @@ from adafruit_pca9685 import PCA9685
 class ServoController:
     SAFE_ANGLES = {
         0: (60, 140),
-        1: (70, 130),
+        1: (0, 180),
         2: (70, 130),
         7: (0,  185),
     }
