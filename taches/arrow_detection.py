6cfb88ed89ffa3b5a4ff0bbd1a4064c5e33da3f0
Félix Lang <felixlang1004@gmail.com>
2026-06-23 11:35:49 +0200

    tests

--- taches/arrow_detection.py
+++ taches/arrow_detection.py
@@ -99,10 +99,10 @@ def main():
 
     SPEED_STRAIGHT = 0.225
 
-    REVERSE_TIME   = 1.6
+    REVERSE_TIME   = 1.55
     RAMP_TIME      = 0.2
 
-    OBSTACLE_DIST_MM = 365
+    OBSTACLE_DIST_MM = 350
 
     sensor    = ultrasonic_sensor.UltrasonicSensor()
     robot     = robot_controller.RobotController(sensor=sensor, auto_watch=False)
