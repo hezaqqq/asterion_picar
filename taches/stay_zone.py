e21b9b3318c03f55dec1f983f2418cd1e5c4d2f9
Félix Lang <felixlang1004@gmail.com>
2026-07-01 16:34:29 +0200

    fix

--- taches/stay_zone.py
+++ taches/stay_zone.py
@@ -7,8 +7,8 @@ import time
 class StayInZone:
 
     ANGLE_CENTER_ROUE = 100
-    ANGLE_MIN_ROUE    = 50
-    ANGLE_MAX_ROUE    = 150
+    ANGLE_MIN_ROUE    = 55
+    ANGLE_MAX_ROUE    = 145
 
     ANGLE_CENTER_TETE_GD = 90
     ANGLE_MIN_TETE_GD    = 18
